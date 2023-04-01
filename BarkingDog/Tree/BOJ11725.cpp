25bcce3fc54bf745b8dd532400de8cbaba09685c
hyowon <hyowon9964@gmail.com>
2023-04-01 13:02:58 +0900

    BOJ11725

---
+++ BarkingDog/Tree/BOJ11725.cpp
@@ -0,0 +1,38 @@
+/*
+  백준 [트리의 부모 찾기]
+  https://www.acmicpc.net/problem/11725
+  트리
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int n;  // 노드 개수
+vector<int> adj[100001];
+int p[100001];
+
+void dfs(int cur){
+  for(int nxt : adj[cur]){
+    if(p[cur] == nxt) continue;
+    p[nxt] = cur;
+    dfs(nxt);
+  }
+}
+
+int main(void){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  
+  cin >> n;
+  for(int i=0; i<n-1; i++){
+    int u, v;
+    cin >> u >> v;
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+  }
+  dfs(1);
+
+  for(int i=2; i<=n; i++) cout << p[i] << "\n";
+}
