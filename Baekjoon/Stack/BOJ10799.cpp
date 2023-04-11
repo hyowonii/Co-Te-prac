da7beec6bfd4f6364cd60efcedcb31a654e1996b
hyowon <hyowon9964@gmail.com>
2023-04-11 13:28:41 +0900

    BOJ10799

--- Baekjoon/Stack/BOJ10799.cpp
+++ Baekjoon/Stack/BOJ10799.cpp
@@ -1,37 +1,37 @@
 /*
   백준 [쇠막대기]
   https://www.acmicpc.net/problem/10799
-  스택
+  stack
 */
 
 #include <iostream>
 #include <stack>
 using namespace std;
 
-stack<char> brackets;
+stack<char> s;
+int result = 0;
 
-int main(void) {
+int main(void){
   ios::sync_with_stdio(0);
-  cin.tie(0);
-
-  int answer = 0;
+  cin.tie(0); cout.tie(0);
 
   string str;
   cin >> str;
 
-  for(int i=0; i<str.length(); i++) {
-    if(str[i] == '(') 
-      brackets.push('(');
-    else {  // 닫는 괄호면
-      brackets.pop();   // 하나 빼고
-
-      if(str[i-1] == '(')   // 레이저
-        answer += brackets.size();  // 스택 사이즈만큼 더하기
-      else  // 쇠막대기의 끝
-        answer += 1;  // 하나 더하기
+  for(int i=0; i<str.size(); i++){
+    if(str[i] == '('){
+      s.push('(');
+    }
+    else{
+      s.pop();
+      if(str[i-1] == '('){ // 레이저
+        result += s.size();
+      }
+      else{
+        result += 1;
+      }
     }
   }
 
-  cout << answer;
-
+  cout << result;
 }
