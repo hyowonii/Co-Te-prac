3ada7c6abc58dd9a1c9b7a59f454ee6c7d15d1b4
hyowon <hyowon9964@gmail.com>
2023-04-11 14:42:19 +0900

    BOJ2283

---
+++ Baekjoon/BOJ2283.cpp
@@ -0,0 +1,41 @@
+/*
+  백준 [구간 자르기]
+  https://www.acmicpc.net/problem/2283
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int n, k;
+int cnt[1000001] = {0,};
+int A=0, B=0;
+
+int main(void){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  cin >> n >> k;
+  while(n--){
+    int a, b;
+    cin >> a >> b;
+    for(int i=a+1; i<=b; i++) cnt[i]++;
+  }
+
+  int st = 0, en = 0;
+  int sum = 0;
+  while(true){
+    if(en == 1000001) break;
+
+    if(sum > k) sum -= cnt[++st];
+    else if(sum < k) sum += cnt[++en];
+    else {
+      A = st;
+      B = en;
+      break;
+    }
+  }
+  
+
+  cout << A << " " << B;
+}
