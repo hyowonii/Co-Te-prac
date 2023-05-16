4c8830a42f45bb4d6e75328492f6c0152c632535
hyowon <hyowon9964@gmail.com>
2023-05-16 19:09:09 +0900

    SWEA1244 fail

---
+++ SS/D3/D3/1244.cpp
@@ -0,0 +1,75 @@
+// 실패
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+string str;
+int n;
+string result;
+
+void init(){
+  cin >> str >> n;
+  result = str;
+}
+
+bool check(){
+  for(int i=0; i<result.size()-1; i++){
+    if(result[i] < result[i+1]) return false;
+  }
+  return true;
+}
+
+void dfs(int idx, int cnt){
+  if(check()) return;
+
+  if(cnt == n){
+    if(stoi(str) > stoi(result)) result = str;
+    return;
+  }
+
+  for(int i=idx; i<str.size(); i++){
+    for(int j = i+1; j<str.size(); j++){
+      char tmp = str[i];
+      str[i] = str[j];
+      str[j] = tmp;
+      dfs(i, cnt+1);
+      tmp = str[i];
+      str[i] = str[j];
+      str[j] = tmp;
+    }
+  }
+}
+
+int main(int argc, char** argv)
+{
+	int test_case;
+	int T;
+	/*
+	   아래의 freopen 함수는 input.txt 를 read only 형식으로 연 후,
+	   앞으로 표준 입력(키보드) 대신 input.txt 파일로부터 읽어오겠다는 의미의 코드입니다.
+	   //여러분이 작성한 코드를 테스트 할 때, 편의를 위해서 input.txt에 입력을 저장한 후,
+	   freopen 함수를 이용하면 이후 cin 을 수행할 때 표준 입력 대신 파일로부터 입력을 받아올 수 있습니다.
+	   따라서 테스트를 수행할 때에는 아래 주석을 지우고 이 함수를 사용하셔도 좋습니다.
+	   freopen 함수를 사용하기 위해서는 #include <cstdio>, 혹은 #include <stdio.h> 가 필요합니다.
+	   단, 채점을 위해 코드를 제출하실 때에는 반드시 freopen 함수를 지우거나 주석 처리 하셔야 합니다.
+	*/
+	//freopen("input.txt", "r", stdin);
+	cin>>T;
+	/*
+	   여러 개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+	*/
+	for(test_case = 1; test_case <= T; ++test_case)
+	{
+    
+    // 초기설정
+    init();
+
+    dfs(0, 0);
+
+    cout << "#" << test_case << " " << result << "\n";
+
+	}
+	return 0;//정상종료시 반드시 0을 리턴해야합니다.
+}
